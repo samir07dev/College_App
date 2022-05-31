1f29d20f8db007f3cfd5c7729bc9d06c5aedf682
Samir07dev <samir07ali@gmail.com>
2022-06-01 00:49:53 +0530

    update

--- app/src/main/java/com/example/adminpanel/MainActivity.java
+++ app/src/main/java/com/example/adminpanel/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.adminpanel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    CardView uploadNotice;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        uploadNotice = findViewById(R.id.addNotice);
+
+        uploadNotice.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.addNotice:
+                Intent intent = new Intent( MainActivity.this,UploadNotice.class);
+                StartActivity(intent);
+                break;
+        }
+
+    }
+
+    private void StartActivity(Intent intent) {
     }
 }
--- app/src/main/java/com/example/adminpanel/UploadNotice.java
+++ app/src/main/java/com/example/adminpanel/UploadNotice.java
@@ -1,14 +1,59 @@
 package com.example.adminpanel;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class UploadNotice extends AppCompatActivity {
+
+    private CardView addImage;
+    private ImageView NoticeImageView;
+
+    private final int REQ = 1;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_notice);
+
+        addImage = findViewById(R.id.addImage);
+        NoticeImageView = findViewById(R.id.NoticeImageView);
+
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent pickimage = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickimage,REQ);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQ && resultCode == RESULT_OK){
+            Uri uri = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            NoticeImageView.setImageBitmap(bitmap);
+        }
     }
 }
